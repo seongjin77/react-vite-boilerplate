49807a0e9f21e8b2f232bb3714db359215d9fe4d
seongjin <kssj9056@gmail.com>
2023-12-25 23:28:36 +0900

    prettier eslint 추가

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -5,14 +5,52 @@ module.exports = {
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
+    "plugin:react/recommended"
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  plugins: ['react-refresh', '@typescript-eslint', 'prettier','simple-import-sort'],
   rules: {
+    'react/react-in-jsx-scope': 'off',
+    'no-var': 'error', // var 금지
+    'no-multiple-empty-lines': 'error', // 여러 줄 공백 금지
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
     ],
+    'prettier/prettier': [
+      'error',
+      {
+        endOfLine: 'auto',
+      },
+    ],
+    '@typescript-eslint/naming-convention': [
+      'error',
+      {
+        format: ['camelCase', 'UPPER_CASE', 'PascalCase'],
+        selector: ['variable'],
+      },
+      {
+        format: ['camelCase', 'PascalCase'],
+        selector: ['parameter'],
+      },
+      {
+        format: ['camelCase'],
+        selector: ['function'],
+      },
+      {
+        format: ['PascalCase'],
+        selector: 'typeLike',
+      },
+    ],
+    'react/function-component-definition': [
+      2,
+      { namedComponents: 'arrow-function' },
+    ],
+    'no-console': 'warn',
+    '@typescript-eslint/array-type': ['error', { default: 'array-simple' }],
+    '@typescript-eslint/consistent-type-imports': 'error',
+    'simple-import-sort/imports': 'error',
+    'simple-import-sort/exports': 'error',
   },
-}
+};
