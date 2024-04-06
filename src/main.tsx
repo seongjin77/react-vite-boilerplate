6c8a01a2d288e4d0c69529675ff9712351200665
seongjin <kssj9056@gmail.com>
2024-04-07 01:32:36 +0900

    react router dom, tailwind 설치

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,10 @@
 import './index.css';
 
-import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { RouterProvider } from 'react-router-dom';
 
-import App from './App.tsx';
+import router from '@/router';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <RouterProvider router={router} />,
 );
